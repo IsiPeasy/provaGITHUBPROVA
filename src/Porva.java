168b01e9d9f900a140616b3512d027ffc6e84904
IsiPeasy <ishak.zehhaf@gmail.com>
2021-05-31 17:39:40 +0200

    pRIMO commit

--- src/Porva.java
+++ src/Porva.java
@@ -1,6 +1,6 @@
 public class Porva {
     public static void main(String[] args) {
         System.out.print("Ciao");
-        //Isi sei un coglione!!!!!
+        //Paul sei un coglione!!!!!
     }
 }
